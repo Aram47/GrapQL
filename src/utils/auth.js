a3fb4eec4acd3c3822e921138d6028ca35934172
Aram47 <aram.minasyan.00@gmail.com>
2025-02-28 14:40:24 +0400

    nothing

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '1h', // Токен действителен 1 час
+    expiresIn: '1h',
   });
 };
 
